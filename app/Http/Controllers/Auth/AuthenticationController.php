a45b0a0031884ba439ea885d8d8ff511306a4290
Precious Oladele <Oladelep77@gmail.com>
2022-11-11 00:53:34 +0100

    initial commit

---
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthenticationController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $request->validated();
+
+        $userData = [
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ];
+
+        $user = User::create($userData);
+        $token = $user->createToken('forumapp')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $request->validated();
+
+        $user = User::whereUsername($request->username)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => 'Invalid credentials'
+            ], 422);
+        }
+
+        $token = $user->createToken('forumapp')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ], 200);
+    }
+}
